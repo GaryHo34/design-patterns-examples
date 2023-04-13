6dfd17c21f5ded533cdc7867996bf9242cf96d22
Gary Ho <garyho0916@gmail.com>
2023-04-13 12:48:20 -0700

    Implement simple factory in python

---
+++ simple-factory/SimpleFactory.py
@@ -0,0 +1,52 @@
+from enum import Enum
+# python 3.8 and above
+from typing import Protocol
+
+class Material(Enum):
+    WOOD = 1
+    STONE = 2
+    IRON = 3
+
+# protocol is a new feature in python 3.8, it is similar to interface in java
+# Note that it is only for type checking, it does not enforce the implementation
+# https://docs.python.org/3/library/typing.html#typing.Protocol
+
+class Axe(Protocol):
+    def getMaterial(self):
+        pass
+
+class WoodAxe(Axe):
+    def getMaterial(self):
+        return "wood"
+
+class StoneAxe(Axe):
+    def getMaterial(self):
+        return "stone"
+
+class IronAxe(Axe):
+    def getMaterial(self):
+        return "iron"
+
+class CraftingFactory:
+    def createAxe(self, material: Material):
+        if material == Material.WOOD:
+            return WoodAxe()
+        elif material == Material.STONE:
+            return StoneAxe()
+        elif material == Material.IRON:
+            return IronAxe()
+        else:
+            raise Exception("Invalid material")
+
+def testCraftingFactory():
+    factory = CraftingFactory()
+    woodAxe = factory.createAxe(Material.WOOD)
+    stoneAxe = factory.createAxe(Material.STONE)
+    ironAxe = factory.createAxe(Material.IRON)
+
+    print("WoodAxe is made of " + woodAxe.getMaterial())
+    print("StoneAxe is made of " + stoneAxe.getMaterial())
+    print("IronAxe is made of " + ironAxe.getMaterial())
+
+if __name__ == "__main__":
+    testCraftingFactory()
